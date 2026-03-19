8f3b66917fe70dda65fa35b3a0b81b6281b620ff
Roger Montes <rogerxmontes@gmail.com>
2026-03-18 21:25:40 -0600

    feat: create default layout with app-navbar component

---
+++ app/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <app-navbar />
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import AppNavbar from "~/components/AppNavbar.vue";
+</script>
+
+<style></style>

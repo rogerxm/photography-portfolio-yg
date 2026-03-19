1f3aa1b5d16e84edfdc02c9ea31e4a1f096c7dd4
Roger Montes <rogerxmontes@gmail.com>
2026-03-18 21:24:29 -0600

    chore: update dependencies in package.json for Nuxt project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,20 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  css: ["./app/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
+  },
+  modules: [
+    "@nuxt/image",
+    "@vueuse/nuxt",
+    "@nuxt/icon",
+    "@nuxtjs/google-fonts",
+  ],
+  googleFonts: {
+    families: {
+      "Playfair Display": [400, 700],
+      Inter: [300, 400, 600],
+    },
   },
 });

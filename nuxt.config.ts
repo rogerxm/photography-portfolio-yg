ee94b6d81b02bef08b0ee0776a0def22fe940a38
Roger Montes <rogerxmontes@gmail.com>
2026-03-25 23:22:07 -0600

    update nuxt.config.ts to include Google Fonts and image modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,18 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  css: ["./app/assets/css/main.css"],
+
   vite: {
     plugins: [tailwindcss()],
+  },
+
+  modules: ["@nuxtjs/google-fonts", "@nuxt/image"],
+  googleFonts: {
+    families: {
+      "Playfair+Display": [400, 700],
+      Inter: [300, 400, 600],
+    },
+    display: "swap",
   },
 });
